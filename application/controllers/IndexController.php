fedc9d1f3ab268129ae3896a10144dc49885ae41
mdymen <msn@dymenstein.com>
2015-06-07 10:58:51 -0300

    inicio

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+class indexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+    
+    public function acabarAction() {}
+    
+}
+
